fb4e87c003c3888ea0413e39d6bfac0da0bdb683
Alexis Bon <alexisbon73@protonmail.com>
2025-09-18 12:51:39 +0200

    Add: pdc preformance plot

---
+++ src/task6_plot_pdc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+
+n_thread_values = set()
+mean_exec_times = {}
+
+with open("../data/results-pdc.csv", "r") as rfile :
+    rfile.readline() # ignore first line
+    for line in rfile:
+        line_data = line.strip().replace(',', '.').split('; ')
+        n_thread_values.add(int(line_data[0]))
+
+        algorithm_name = line_data[1]
+        mean_exec_time = float(line_data[2]) / 1000000000 # convert execution time in seconds
+
+        if (algorithm_name not in mean_exec_times):
+            mean_exec_times[algorithm_name] = []
+        mean_exec_times[algorithm_name].append(mean_exec_time)
+
+n_thread_values = list(n_thread_values)
+n_thread_values.sort()
+y = [mean_exec_times["Sequential"][0] for _ in n_thread_values]
+print(n_thread_values)
+print(y)
+
+plt.plot(n_thread_values, y, label="Sequential")
+
+for algorithm in mean_exec_times:
+    if (algorithm == "Sequential"):
+        continue
+    if (algorithm == "ForkJoinPool"):
+        plt.plot([1, 2, 4, 8, 16], mean_exec_times[algorithm], label=algorithm)
+    else :
+        plt.plot(n_thread_values, mean_exec_times[algorithm], label=algorithm)
+
+plt.xlabel("number of threads used")
+plt.ylabel("mean computation time (s)")
+plt.title("Sort performance on a 10M array on PDC")
+plt.legend()
+plt.show()
